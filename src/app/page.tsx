11a7f3e8562efd8d68f52f3e798f9d9ba0baddb7
imsuryya <suryar12130@gmail.com>
2025-02-18 22:11:01 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Navbar from "../components/navbar"
-import { TextHoverEffectDemo } from "./hero/page"
+import { Hero } from "./pages/hero"
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
-      <TextHoverEffectDemo/>
+      <Hero/>
 
       {/* Add content for scroll testing */}
       <div className="pt-32 px-4 max-w-7xl mx-auto">
--- src/app/pages/hero.tsx
+++ src/app/pages/hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
-export function TextHoverEffectDemo() {
+export function Hero() {
   return (
     <div className="h-screen flex items-center justify-center">
       <TextHoverEffect text="SRM IST" />
